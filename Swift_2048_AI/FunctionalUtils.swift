1f5409486248322d1286da03b63b70f9cece7d5f
tranlynhathao <haotran04022005@gmail.com>
2025-05-02 17:21:32 +0700

    feat: add basic content

---
+++ Swift_2048_AI/FunctionalUtils.swift
@@ -0,0 +1,10 @@
+//
+//  Swift_2048_AIApp.swift
+//  Swift_2048_AI
+//
+//  Created by Trần Lý Nhật Hào on 2/5/25.
+//
+
+func bind<T, U>(_ x: T, _ closure: (T) -> U) -> U {
+    return closure(x)
+}
